35a3837a11b406cb83036329e449691143e5181f
Allan Gabriel Ramos da Silva <allan.silva@rain.us>
2022-06-03 16:38:28 -0300

    chore(eslint): add react and typescript plugins

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
+import * as React from 'react'
 import logo from './logo.svg'
-import './App.css';
+import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = React.useState(0)
 
   return (
     <div className="App">

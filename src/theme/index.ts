5d0d35f486ae926b75fa15a1e5c8cf3700a224d1
Allan Gabriel Ramos da Silva <allan.silva@rain.us>
2022-06-09 20:12:12 -0300

    chore(chakra): add reset css

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -5,12 +5,29 @@ import { colors } from './colors'
 const theme = extendTheme({
   styles: {
     global: {
-      body: {
-        padding: '0 !important',
-        minHeight: '-webkit-fill-available',
+      '*, *::before, *::after': {
+        'box-sizing': 'border-box',
+        margin: 0,
       },
-      html: {
-        height: '-webkit-fill-available',
+      'html, body': {
+        height: '100%',
+      },
+      body: {
+        'line-height': 1.5,
+        '-webkit-font-smoothing': 'antialiased',
+      },
+      'img, picture, video, canvas, svg': {
+        display: 'block',
+        'max-width': '100%',
+      },
+      'input, button, textarea, select': {
+        font: 'inherit',
+      },
+      'p, h1, h2, h3, h4, h5, h6': {
+        'overflow-wrap': 'break-word',
+      },
+      '#root, #__next': {
+        isolation: 'isolate',
       },
     },
   },
